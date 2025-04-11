3a6340efa7e53016bda1607075ce04a2fd62d6ea
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-11 12:50:55 +0200

    init project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elenco dischi</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <!-- /BOOTSTRAP -->
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    
+</body>
+</html>
