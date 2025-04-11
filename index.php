15bb43011a0183cf0f487cdd06d5a5ca12e0410e
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-11 19:02:16 +0200

    add comments - delete files functions.php and style.css

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$disksText = file_get_contents("./disks.json");
-$disks = json_decode($disksText, true);
+$disksText = file_get_contents("./disks.json"); // prendo i dati da disks.json 
+$disks = json_decode($disksText, true); // trasformo il json in struttura php
 
 ?>
 
@@ -14,7 +14,6 @@ $disks = json_decode($disksText, true);
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <!-- /BOOTSTRAP -->
-    <link rel="stylesheet" href="./style.css">
 </head>
 <body class="bg-dark">
     <header class="bg-black">
--- server.php
+++ server.php
@@ -1,10 +1,11 @@
 <?php
-
+// controllo se il metodo è POST e se tutti i valori sono impostati, se è tutto corretto procedo
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["title"], $_POST["artist"], $_POST["url"], $_POST["year"], $_POST["genre"])) {
 
-    $disksText = file_get_contents("./disks.json");
-    $disks = json_decode($disksText, true);
+    $disksText = file_get_contents("./disks.json"); // prendo i dati da disks.json 
+    $disks = json_decode($disksText, true); // trasformo il json in struttura php
 
+    // creo il nuovo disco in array php con valori presi dal form su index.php (metodo post)
     $newDisk = [
         "titolo" => $_POST["title"],
         "artista" => $_POST["artist"],
@@ -13,10 +14,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["title"], $_POST["arti
         "genere" => $_POST["genre"]
     ];
 
-    $disks[] = $newDisk;
+    $disks[] = $newDisk; // nell'array con tutti i dischi aggiungo ([]) il nuovo disco
 
-    $disksUpdated = json_encode($disks, JSON_PRETTY_PRINT);
-    file_put_contents("./disks.json", $disksUpdated);
+    $disksUpdated = json_encode($disks, JSON_PRETTY_PRINT); // converto la struttura php in json (json_pretty_print serve a rendere json più leggibile e non in un'unica riga)
+    file_put_contents("./disks.json", $disksUpdated); // salvo il nuovo elenco di dischi in disks.json sovrascrivendolo
 }
 
 header("Location: index.php");
